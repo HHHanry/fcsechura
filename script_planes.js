d7fd67d45fe06e9a1b5adafeedff5518860c521f
HHHanry <hanryzaf@gmail.com>
2025-02-01 14:16:05 -0500

    Update script_planes.js

--- script_planes.js
+++ script_planes.js
@@ -1,6 +1,6 @@
  function sendWhatsAppMessage(planName) {
     const phoneNumber = "51947300694";
-    const message = `Buen día FC SECHYRA. ¡Estoy interesado en adquirir el plan ${planName} ! ¿Me podría brindra más información?`;
+    const message = `Buen día FC SECHURA. ¡Estoy interesado en adquirir el plan ${planName} ! ¿Me podría brindrar más información?`;
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   }
